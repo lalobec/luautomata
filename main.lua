10ce8779529b4defbd78eb645517e9902e2cc393
lalobgc <lalobg.c@gmail.com>
2025-04-11 13:36:52 -0600

    left clic now gives life to cells

--- main.lua
+++ main.lua
@@ -27,8 +27,10 @@ function love.draw()
     local cell_ypos = cell_size * (y-1)
     for x = 1, grid_x do
       local cell_xpos = cell_size * (x-1)
-      if grid[y][x] then
+      if x == cursor_atXcell and y == cursor_atYcell then
         love.graphics.setColor(love.math.colorFromBytes(61, 141, 122))
+      elseif grid[y][x] then
+        love.graphics.setColor(love.math.colorFromBytes(61, 101, 122))
       else
         love.graphics.setColor(0.86, 0.86, 0.86)
       end
@@ -42,12 +44,16 @@ function love.draw()
   love.graphics.print('Mouse y position: '..cursor_atYcell, 0, 20)
 
   -- Highlight the cell at cursor position
-  love.graphics.setColor(love.math.colorFromBytes(61, 141, 122))
-  love.graphics.rectangle("fill", (cursor_atXcell-1)*cell_size, (cursor_atYcell-1)*cell_size, cell_size-1, cell_size-1)
+  -- love.graphics.setColor(love.math.colorFromBytes(61, 141, 122))
+  -- love.graphics.rectangle("fill", (cursor_atXcell-1)*cell_size, (cursor_atYcell-1)*cell_size, cell_size-1, cell_size-1)
+  -- Now implemented with an if statement
 end
 
 function love.update()
   cursor_atXcell = math.floor(love.mouse.getX() / cell_size) + 1
   cursor_atYcell = math.floor(love.mouse.getY() / cell_size) + 1
+  if love.mouse.isDown(1) then
+    grid[cursor_atYcell][cursor_atXcell] = true
+  end
 end
 
