003aa1ae705147cb48332366b35692dc572679d4
lalobgc <lalobg.c@gmail.com>
2025-04-30 10:07:43 -0600

    Separated code, grid.lua now contains the generation of the grid and the algorithm for Conways game

--- grid.lua
+++ grid.lua
@@ -1,14 +1,69 @@
-local params = require("params")
+local params = require("parameters")
 
-love.graphics.setBackgroundColor(1, 1, 1)
-grid_y = params.win_height / params.cell_size - 5
-grid_x = params.win_width / params.cell_size
+local Grid = {}
 
-  -- creating the grid:
-  for j = 0, grid_y do
-    grid[j] = {}
-    for i = 0, grid_x do
-      grid[j][i] = false
+function Grid.create_grid()
+	-- creating the grid:
+  local grid = {}
+	for j = 0, params.grid_y do
+		grid[j] = {}
+		for i = 0, params.grid_x do
+			grid[j][i] = false
+		end
+	end
+  return grid
+end
+
+function Grid.draw_grid(grid, cursor_xcell, cursor_ycell)
+  for y = 1, params.grid_y do
+    local cell_ypos = params.cell_size * (y-1)
+    for x = 1, params.grid_x do
+      local cell_xpos = params.cell_size * (x-1)
+      if x == cursor_xcell and y == cursor_ycell then
+        love.graphics.setColor(love.math.colorFromBytes(61, 141, 122))
+      elseif grid[y][x] then
+        love.graphics.setColor(love.math.colorFromBytes(61, 101, 122))
+      else
+        love.graphics.setColor(0.86, 0.86, 0.86)
+      end
+      love.graphics.rectangle("fill", cell_xpos, cell_ypos, params.cell_size-1, params.cell_size-1)
     end
   end
+end
 
+local function count_neighbors(grid, y, x)
+  local neighbors = 0
+  for dy = -1, 1 do
+    for dx = -1, 1 do
+      if not (dy == 0 and dx == 0) and grid[y+dy] and grid[y+dy][x+dx] then
+          neighbors = neighbors + 1
+      end
+    end
+  end
+  return neighbors
+end
+
+
+function Grid.next_grid(grid)
+  local nextGrid = {}
+    for y = 1, params.grid_y do
+      nextGrid[y] = {}
+      for x = 1, params.grid_x do
+        local neighbors = count_neighbors(grid, y, x)
+        if grid[y][x] then
+          if neighbors < 2 or neighbors > 3 then
+            nextGrid[y][x] = false
+          else
+            nextGrid[y][x] = true
+          end
+        elseif not grid[y][x] and neighbors == 3 then
+          nextGrid[y][x] = true
+        end
+      end
+    end
+  return nextGrid
+end
+
+
+
+return Grid
--- main.lua
+++ main.lua
@@ -1,51 +1,39 @@
 local params = require("parameters")
 local gen_button = require("generic_button")
+local Grid = require("grid")
 local cursor_atXcell
 local cursor_atYcell
-local grid_y
-local grid_x
 
-local grid = {}
 local neighbors = 0
+local grid = Grid.create_grid()
 
-
+-- Buttons
 local clear_button = gen_button.new(20, params.win_height-22, 110, 20, "Clear The Grid")
 local b2 = gen_button.new(140, params.win_height-22, 100, 20, "Test button")
 
+-- --------------
+-- Love.load
+-- --------------
 function love.load()
   love.window.setTitle("Conway's Game of Life")
   love.window.setMode(params.win_width, params.win_height)
   love.keyboard.setKeyRepeat(true)
   love.graphics.setBackgroundColor(1, 1, 1)
-
-  -- creating the grid:
-  for j = 0, params.grid_y do
-    grid[j] = {}
-    for i = 0, params.grid_x do
-      grid[j][i] = false
-    end
-  end
 end
 
+
+-- --------------
+-- Love.draw
+-- --------------
 function love.draw()
   clear_button:draw()
-  for y = 1, params.grid_y do
-    local cell_ypos = params.cell_size * (y-1)
-    for x = 1, params.grid_x do
-      local cell_xpos = params.cell_size * (x-1)
-      if x == cursor_atXcell and y == cursor_atYcell then
-        love.graphics.setColor(love.math.colorFromBytes(61, 141, 122))
-      elseif grid[y][x] then
-        love.graphics.setColor(love.math.colorFromBytes(61, 101, 122))
-      else
-        love.graphics.setColor(0.86, 0.86, 0.86)
-      end
-      love.graphics.rectangle("fill", cell_xpos, cell_ypos, params.cell_size-1, params.cell_size-1)
-    end
-  end
+  Grid.draw_grid(grid, cursor_atXcell, cursor_atYcell)
   b2:draw()
 end
 
+-- --------------
+-- Love.update
+-- --------------
 function love.update()
   clear_button:hover()
   b2:hover()
@@ -75,35 +63,8 @@ function love.keypressed(key)
     love.event.quit()
   elseif key == "space" then
     print("space pressed")
-    local nextGrid = {}
-    for y = 1, params.grid_y do
-      nextGrid[y] = {}
-      for x = 1, params.grid_x do
-        neighbors = count_neighbors(y, x)
-        if grid[y][x] then
-          if neighbors < 2 or neighbors > 3 then
-            nextGrid[y][x] = false
-          else
-            nextGrid[y][x] = true
-          end
-        elseif not grid[y][x] and neighbors == 3 then
-          nextGrid[y][x] = true
-        end
-      end
-    end
-    grid = nextGrid
+    grid = Grid.next_grid(grid)
   end
 end
 
-function count_neighbors(y, x)
-  neighbors = 0
-  for dy = -1, 1 do
-    for dx = -1, 1 do
-      if not (dy == 0 and dx == 0) and grid[y+dy] and grid[y+dy][x+dx] then
-          neighbors = neighbors + 1
-      end
-    end
-  end
-  return neighbors
-end
 
